af119dd0f6e0517b3ab07ee2efdc9b18dc488353
LENOVO-LAPTOP <7720585+nono-london@users.noreply.github.com>
2023-12-23 13:49:27 +0100

    v0.0.1.8

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name="proxy_helpers",
     packages=find_packages(),
-    version="0.0.1.7",
+    version="0.0.1.8",
     description="Handles proxies and database access",
     author="Nono London",
     author_email="",
