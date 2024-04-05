8ecd2300ad1c2f94bd1c930d1f0faf3f9e348584
hplaptop <7720585+nono-london@users.noreply.github.com>
2024-04-05 10:31:34 +0200

    added proxy web name to the dict test

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name="proxy_helpers",
     packages=find_packages(),
-    version="0.0.2.1",
+    version="0.0.2.2",
     description="Handles proxies and database access",
     author="Nono London",
     author_email="",
