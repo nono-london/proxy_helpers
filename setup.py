4d73339130c5503d049817ff5a2370428dbd7f63
hplaptop <7720585+nono-london@users.noreply.github.com>
2024-04-16 12:11:29 +0200

    corrected async errors

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name="proxy_helpers",
     packages=find_packages(),
-    version="0.0.2.3",
+    version="0.0.2.3.1",
     description="Handles proxies and database access",
     author="Nono London",
     author_email="",
